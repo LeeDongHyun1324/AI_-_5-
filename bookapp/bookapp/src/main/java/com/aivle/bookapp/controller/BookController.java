9c8c21be93b03efeddc5495b02f96ff6a7339c41
vitaminwoo <nwpark111@gmail.com>
2026-06-09 15:22:45 +0900

    feat: BookController 뼈대 정의

--- bookapp/bookapp/src/main/java/com/aivle/bookapp/controller/BookController.java
+++ bookapp/bookapp/src/main/java/com/aivle/bookapp/controller/BookController.java
@@ -1,13 +1,36 @@
 package com.aivle.bookapp.controller;
 
+import com.aivle.bookapp.domain.Book;
 import com.aivle.bookapp.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
+
+    @GetMapping("/api/books")
+    public List<Book> getAllBooks() {}
+
+    @GetMapping("/api/books/{id}")
+    public Book getBook(@PathVariable Long id) {}
+
+    @PostMapping("/api/books")
+    public ResponseEntity<Book> createBook(@Valid @RequestBody Book book) {}
+
+    @PatchMapping("/api/books/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {}
+
+    @DeleteMapping("/api/books/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {}
+
+    @PatchMapping("/api/books/{id}/cover")
+    public Book updateBookCover(@PathVariable Long id, @RequestBody Book book) {}
 
 }
